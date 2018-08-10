ae4ac9676a8fcfc70211f61bed462e7a7aa34eb1
Ficks <15111327689@163.com>
2018-08-10 11:00:38 +0800

    1111111

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '192.168.0.107', // can be overwritten by process.env.HOST
+    host: '192.168.31.99', // can be overwritten by process.env.HOST
     port: 81, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
